93abc55561bbf5f4135f9a5900ec2f875b01aaf8
Dante Fernando <dante.fernando@sensiaglobal.com>
2025-06-03 11:34:23 -0600

    added main.py, doesn't work

---
+++ main.py
@@ -0,0 +1,26 @@
+# Allow user to interactively connect and disconnect usb devices using usbipd
+
+from subprocess import check_output
+
+
+
+# get devices
+def getDevices():
+    pass
+
+
+
+
+
+# Give the user the option to choose between devices
+def menu():
+    print
+
+
+
+def main():
+    menu()
+
+
+if __name__ == "__main__":
+    main()
